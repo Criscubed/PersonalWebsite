a43af8c2f692b2290f532ff0e0c35d68b4e3c8ee
Criscubed <criscubed@gmail.com>
2019-01-31 21:49:06 -0600

    Cleaned up the code a little bit

--- Lots of colors/sketch.js
+++ Lots of colors/sketch.js
@@ -14,59 +14,59 @@ var tendrilLength = 20;
 
  
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  canvasSize = windowWidth;
-  circleSize = canvasSize/20;
-  noStroke();
-  red = random(0, 255);
+	createCanvas(windowWidth, windowHeight);
+	canvasSize = windowWidth;
+	circleSize = canvasSize/20;
+	noStroke();
+	red = random(0, 255);
 	green = random(0, 255);
-  blue = random (0, 255);
+	blue = random (0, 255);
 }
 
 function draw() {
 	for(var i = 0; i < 5; i++){
 		chooseColors();
-  	changeColors();
+		changeColors();
 		circlePos(color);
 	}
-  if(tendrils.length > 1){
-    for(var i = 0; i < tendrils.length; i++){
-    	var monster = tendrils.shift();
-  		monster.draw();
-    	if(monster.i > 0){
-    		tendrils.push(monster);
-    	}
-    }
-  }
+	if(tendrils.length > 1){
+		for(var i = 0; i < tendrils.length; i++){
+			var monster = tendrils.shift();
+			monster.draw();
+			if(monster.i > 0){
+				tendrils.push(monster);
+			}
+		}
+	}
 }
 
 function circlePos(color){
-  var xCoord = map(color, 0, 255, 0, canvasSize);
+    var xCoord = map(color, 0, 255, 0, canvasSize);
 	var yCoord = random(0, windowHeight);
 	ellipse(xCoord, yCoord, circleSize, circleSize);
 }
 
 function chooseColors(){
 	red = restrict(red + (random(50) * (round(random()) * 2 - 1)), 0, 255);
-  green = restrict(green + (random(50) * (round(random()) * 2 - 1)), 0, 255);
-  blue = restrict(blue + (random(50) * (round(random()) * 2 - 1)), 0, 255);
+	green = restrict(green + (random(50) * (round(random()) * 2 - 1)), 0, 255);
+	blue = restrict(blue + (random(50) * (round(random()) * 2 - 1)), 0, 255);
 	fill(red, green, blue);  
 }
 
 function restrict (x){
 	if(x > 255){
-  	return random(205, 255);  
-  }
-  if(x < 0){
-  	return random(0, 50);
-  }
-  return x;
+		return random(205, 255);  
+	}
+	if(x < 0){
+		return random(0, 50);
+	}
+	return x;
 }
 
 
 
 function changeColors(){
-  switch(toggle){
+	switch(toggle){
   		case 0:
  	  		color = red;
     		break;
@@ -76,68 +76,68 @@ function changeColors(){
  	    case 2:
  	      color = blue; 
  	      break;
- 	 	}  
+ 	}  
 }
 
 function Tendril(){
-  this.x = mouseX;
-  this.y = mouseY;
-  this.i = tendrilLength;
-  this.dirX = round(random()) * 2 - 1;
-  this.dirY = round(random()) * 2 - 1;
-  this.draw = function(){
-    tendrilColors(this.x);
-  	ellipse(this.x, this.y, circleSize, circleSize);
-    this.x = this.x + random(circleSize/4 ,circleSize/4 * 3) * this.dirX;
- 	  this.y = this.y + random(circleSize/4 ,circleSize/4 * 3) * this.dirY;
-    this.i--;
-    this.dirX = changeSign(this.dirX);
-  	this.dirY = changeSign(this.dirY);
-  }
+	this.x = mouseX;
+	this.y = mouseY;
+	this.i = tendrilLength;
+	this.dirX = round(random()) * 2 - 1;
+	this.dirY = round(random()) * 2 - 1;
+	this.draw = function(){
+		tendrilColors(this.x);
+		ellipse(this.x, this.y, circleSize, circleSize);
+		this.x = this.x + random(circleSize/4 ,circleSize/4 * 3) * this.dirX;
+		this.y = this.y + random(circleSize/4 ,circleSize/4 * 3) * this.dirY;
+		this.i--;
+		this.dirX = changeSign(this.dirX);
+		this.dirY = changeSign(this.dirY);
+	}
 }
 
 function tendrilColors(x){
-  switch(toggle){
-    case 0:
+	switch(toggle){
+		case 0:
 			fill(map(x, 0, windowWidth, 0, 255), random(0,255), random(0,255));
-      break;
-    case 1:
-      fill(random(0,255), map(x, 0, windowWidth, 0, 255), random(0,255));
-      break;
-    case 2:
-      fill(random(0, 255), random(0, 255), map(x, 0, windowWidth, 0, 255));
-      break;
+			break;
+		case 1:
+			fill(random(0,255), map(x, 0, windowWidth, 0, 255), random(0,255));
+			break;
+		case 2:
+			fill(random(0, 255), random(0, 255), map(x, 0, windowWidth, 0, 255));
+			break;
 	}
 }
 function changeSign(num){
 	if(random() > 0.9){
-  	return num * -1;  
-  } else {
-    return num;
-  }
+		return num * -1;  
+	} else {
+		return num;
+	}
 }
 
 function mousePressed(){
 	toggle++;
-  toggle = toggle%3;
-  for(var i = 0; i < tendrilsPerMonster; i++){
+	toggle = toggle%3;
+	for(var i = 0; i < tendrilsPerMonster; i++){
 		tendrils.push(new Tendril());  
-  }
+	}
 }
 
 function keyPressed(){
-  if(strokeToggle){
-  	stroke(0);
-  } else {
-    noStroke(); 
-  }
-  strokeToggle = !strokeToggle;
+	if(strokeToggle){
+		stroke(0);
+	} else {
+		noStroke(); 
+	}
+	strokeToggle = !strokeToggle;
 }
 
 
 function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
-  canvasSize = windowWidth;
-  circleSize = canvasSize/13;
-  print(windowWidth/windowHeight);
+	resizeCanvas(windowWidth, windowHeight);
+	canvasSize = windowWidth;
+	circleSize = canvasSize/13;
+	print(windowWidth/windowHeight);
 }
